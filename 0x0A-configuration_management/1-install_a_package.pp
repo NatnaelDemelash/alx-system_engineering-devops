29ae4cf021ef2f1478f27fabaa813c1800872a66
NatnaelDemelash <natidemelash074@gmail.com>
2022-09-16 02:23:06 -0700

    Update file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install the package puppet-lint 2.5.2
-
-package { 'puppet-lint':
-    ensure   => '2.5.2',
-    provider => 'gem',
-    }
+# install puppet-lint
+package{'puppet-lint':
+ensure   => '2.5.0',
+provider => 'gem'
+}
